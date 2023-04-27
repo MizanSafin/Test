50e4ebfc5d2e4c07b135e1fd3ff9659c5f81d095
MizanSafin <drmizanurrahman452@gmail.com>
2023-04-27 16:50:04 +0600

    3rd modification

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 
 const arr = ["a","quick","brown","fox"]
-let result = "";
-for (let i = 0; i < arr.length; i++) {
-      if(arr[i].length > result.length)   {
-        result = arr[i]+"..."
-      }
-}
-console.log(result)
+
+const nums = [2,3,4];
+const nums2 = [1,12,34,55,56]
+const localArr = nums.slice()
+ nums2.splice(1,0,...nums)
+console.log(nums2 ,localArr)
